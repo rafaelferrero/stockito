8d794896d024df9c915842e11215304f32b032c8
Rafael Ferrero <rafael.ferrero@gmail.com>
2018-04-05 09:14:19 -0300

    creo el modelo para multiple depositos y ubicaciones dependientes.

--- almacenes/models.py
+++ almacenes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Deposito(models.Model):
+    deposito = models.ForeignKey(
+        'Deposito',
+        verbose_name="Depósito Padre",
+        blank=True,
+    )
+    identificacion = models.CharField(
+        max_length=255,
+        verbose_name="Identificación del Depósito",
+        help_text="Ej. Estantería 01",
+    )
+
+    def __str__(self):
+        deposito=""
+        if self.deposito:
+            deposito = " (en {})".format(self.deposito)
+
+        return "{}{}".format(self.identificacion,deposito)
+
+
