7e8d488b4b16614d5eb0d8925f3c169d72d2f72a
Rafael Ferrero <rafael.ferrero@gmail.com>
2018-04-05 14:40:27 -0300

    Se ordenaron los campos, y se adiciono list_display, list_filter y search_fields

--- stockito/entidades/admin.py
+++ stockito/entidades/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 from entidades.models import Proveedor, Cliente
 
 
@@ -9,4 +10,36 @@ class ProveedorAdmin(admin.ModelAdmin):
 
 @admin.register(Cliente)
 class ClienteAdmin(admin.ModelAdmin):
-    pass
+    actions_on_bottom = True
+    fieldsets = (
+        (None, {
+            'fields': (
+                (
+                    'apellido',
+                    'nombre',
+                 ),
+                (
+                    'telefono',
+                    'email',
+                ),
+            )
+        }),
+    )
+    list_display = (
+        'nombre_completo',
+        'telefono',
+        'email',
+    )
+    list_filter = (
+        'apellido',
+    )
+    search_fields = (
+        'apellido',
+        'nombre',
+        'telefono',
+        'email',
+    )
+
+    def nombre_completo(self, obj):
+        return obj
+    nombre_completo.short_description = _('Apellido y Nombre')
