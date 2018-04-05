c03f574f12275ee1e505f1466f2da8b56f2ae565
Rafael Ferrero <rafael.ferrero@gmail.com>
2018-04-05 09:41:28 -0300

    agrego los articulos, ubicaciones y los related_names a las relaciones

--- almacenes/models.py
+++ almacenes/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from entidades.models import Proveedor
 
 
 class Deposito(models.Model):
     deposito = models.ForeignKey(
         'Deposito',
+        related_name="deposito_deposito",
         verbose_name=_("Depósito Padre"),
         blank=True,
     )
@@ -22,3 +24,47 @@ class Deposito(models.Model):
         return "{}{}".format(self.identificacion, deposito)
 
 
+class Articulo(models.Model):
+    codigo = models.CharField(
+        max_length=255,
+        verbose_name=_("Identificador"),
+        help_text=_("Código de Identificación Interna"),
+    )
+    descripcion = models.CharField(
+        max_length=255,
+        verbose_name=_("Descripción"),
+        help_text=_("Descripción del Artículo"),
+    )
+    proveedor = models.ForeignKey(
+        Proveedor,
+        related_name="proveedor_articulo",
+        verbose_name=_("Proveedor"),
+        help_text=_(
+            "Si el mismo artículo lo proveen empresas distintas"
+            " es preferible cargar dos artículos distintos"
+        )
+    )
+
+    def __str__(self):
+        return "{} - {}".format(
+            self.codigo,
+            self.descripcion,
+        )
+
+
+class Ubicacion(models.Model):
+    ubicacion = models.ForeignKey(
+        Deposito,
+        verbose_name=_("Ubicación."),
+        help_text=_("Ubicación donde se almacena el artículo en el Depósito"),
+        related_name="deposito_ubicacion",
+        blank=True,
+    )
+    articulo = models.ForeignKey(
+        Articulo,
+        verbose_name=_("Artículo"),
+        related_name="articulo_ubicacion",
+    )
+
+    def __str__(self):
+        return "{}".format(self.ubicacion.identificacion)
