a88b8f260e64da1c57fdd9b8564c964de1e2ec80
Rafael Ferrero <rafael.ferrero@gmail.com>
2018-04-05 12:14:27 -0300

    Cientos de correcciones para poder hacer andar el modelo.

--- stockito/documentos/models.py
+++ stockito/documentos/models.py
@@ -3,11 +3,12 @@ from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 from entidades.models import Proveedor, Cliente
 from almacenes.models import Movimiento
+from documentos.choices import LETRA
 
 
 class Documento(models.Model):
     fecha = models.DateField(
-        default=timezone.now(),
+        default=timezone.now,
         verbose_name=_("Fecha"),
         help_text=_("Fecha de la transacción"),
     )
@@ -25,11 +26,15 @@ class Documento(models.Model):
 class DocumentoIngreso(Documento):
     proveedor = models.ForeignKey(
         Proveedor,
-        related_name="proveedor_ingreso",
+        related_name="proveedores_documentoingreso",
+        related_query_name="proveedor_documentoingreso",
         verbose_name=_("Proveedor"),
+        on_delete=models.PROTECT,
     )
     letra = models.CharField(
-        choices=['A', 'B', 'C', 'E'],
+        max_length=1,
+        choices=LETRA,
+        default=LETRA[0][0],
         help_text=_("Letra de la factura de Compra")
     )
     punto_venta = models.CharField(
@@ -61,9 +66,11 @@ class DocumentoIngreso(Documento):
 class DocumentoEgreso(Documento):
     cliente = models.ForeignKey(
         Cliente,
-        related_name="cliente_egreso",
+        related_name="clientes_documentoegreso",
+        related_query_name="cliente_documentoegreso",
         verbose_name=_("Cliente"),
-        help_text=_("Persona que solicita el artículo")
+        help_text=_("Persona que solicita el artículo"),
+        on_delete=models.PROTECT,
     )
     motivo = models.CharField(
         max_length=500,
@@ -82,7 +89,9 @@ class Ingreso(Movimiento):
     documento = models.ForeignKey(
         DocumentoIngreso,
         verbose_name=_("Documento de Ingreso"),
-        related_name="documento_ingreso"
+        related_name="documentos_ingreso",
+        related_query_name="documento_ingreso",
+        on_delete=models.PROTECT,
     )
 
 
@@ -90,5 +99,7 @@ class Egreso(Movimiento):
     documento = models.ForeignKey(
         DocumentoEgreso,
         verbose_name=_("Documento de Egreso"),
-        related_name="documento_egreso"
+        related_name="documentos_egreso",
+        related_query_name="documento_egreso",
+        on_delete=models.PROTECT,
     )
